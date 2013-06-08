747387bc9f8a4fe0dc35236de02977e9fa7f868f
Nikolaj Leischner <nikolaj.leischner@gmail.com>
2013-06-08 15:59:21 +0200

    Added scalameter, and a skeleton benchmark.

---
+++ src/test/scala/org/samplesort/SortBenchmark.scala
@@ -0,0 +1,34 @@
+package org.samplesort
+
+import org.scalameter.api._
+import collection.mutable.ArrayBuffer
+
+object SortBenchmark
+extends PerformanceTest.Quickbenchmark {
+  val sizes = Gen.exponential("size")(2 << 12, 2 << 19, 2)
+  var r = new scala.util.Random
+  
+  val iterations = for {
+    size <- sizes 
+  } yield size
+  
+  performance of "RNG" in {
+    measure method "nextInt" in {
+      using(iterations) in {
+        i => for (count <- Range(0, i)) r.nextInt
+      }
+    }
+  }
+
+  val arrays = for {
+    size <- sizes
+  } yield ArrayBuffer.fill(size)(r.nextInt)
+
+  performance of "Sort" in {
+    measure method "sort" in {
+      using(arrays) in {
+        a => a.sorted
+      }
+    }
+  }
+}
